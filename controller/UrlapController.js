2b263029884339dee484bb48ccd6504d6afedbce
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-28 13:18:37 +0100

    felesleges console.log kiszedése

--- controller/UrlapController.js
+++ controller/UrlapController.js
@@ -3,7 +3,6 @@ import UrlapView from "../view/UrlapView.js"
 
 export default class UrlapController {
     constructor(){
-        console.log("controller")
         new UrlapView($(".urlap"))
 
         $(window).on("ujAdatHozzaadasa", (event)=>{
