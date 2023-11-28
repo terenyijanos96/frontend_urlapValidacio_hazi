d551c2624a2dcd393e83d058496e90b9d2e89b96
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-28 19:59:22 +0100

    model elkészítése

--- model/UrlapModel.js
+++ model/UrlapModel.js
@@ -1,6 +1,41 @@
 export default class UrlapModel {
-    constructor(){
+  constructor() {
+    axios.defaults.baseURL = "http://localhost:8000/api/";
+  }
 
+  async get(uri, callback) {
+    try {
+      const response = await axios(uri);
+      callback(response);
+    } catch (error) {
+      console.log(error);
     }
-    
+  }
+
+  async post(uri, data) {
+    try {
+      const response = await axios(uri, data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async put(uri, id, data) {
+    try {
+      const response = await axios(uri + "/" + id, data);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async delete(uri, id) {
+    try {
+      const response = await axios(uri + "/" + id);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
