a0aded7974fdf7e4698a19fc4924bfa05ceca7ca
terenyijanos96 <terenyi.janos96@gmail.com>
2023-12-01 12:35:57 +0100

    Controller kialakítása

--- backend/app/Http/Controllers/PersonController.php
+++ backend/app/Http/Controllers/PersonController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    //
+
+    public function index(){
+        return response()->json(Person::all());
+    }
+
+    public function show($id){
+        return response()->json(Person::find($id));
+    }
+
+    public function store(Request $request){
+        $person = new Person();
+        $person->last_name = $request->last_name;
+        $person->first_name = $request->first_name;
+        $person->birth_year = $request->birth_year;
+        $person->save();
+    }
 }
