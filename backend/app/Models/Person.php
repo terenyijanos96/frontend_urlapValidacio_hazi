8b2fc3d6a78e15b9f5b8fc1afbd43af506f4e922
terenyijanos96 <terenyi.janos96@gmail.com>
2023-12-01 12:30:13 +0100

    Model fájl kitöltése

--- backend/app/Models/Person.php
+++ backend/app/Models/Person.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'last_name',
+        'first_name',
+        'birth_year',
+    ]
 }
